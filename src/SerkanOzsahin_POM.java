f24dcfbd483d33ca9508d93d8172d98785386fd1
Serkan <ozsahinserkan@gmail.com>
2023-09-16 00:31:42 +0300

    tabMenuSubMenuLink ve tabMenuSubMenu yaratıldı ve kontrol edildi

--- src/SerkanOzsahin_POM.java
+++ src/SerkanOzsahin_POM.java
@@ -1,19 +1,23 @@
+import Utility.BaseDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
 public class SerkanOzsahin_POM {
 
+    public SerkanOzsahin_POM() {PageFactory.initElements(BaseDriver.driver, this);}
 
+    @FindBy(css = "ul[class='top-menu notmobile'] > li")
+    public List<WebElement> tabMenuListCheck;
 
+    @FindBy(css = "div[class='page-title'] > h1")
+    public WebElement textCheck;
 
+    @FindBy(css = "ul[class='top-menu notmobile'] ul[class='sublist first-level'] a")
+    public List<WebElement> subMenuList;
 
-
-
-
-
-
-
-
-
-
-
-
-
+    @FindBy(css = "div[class='page-title'] > h1")
+    public WebElement subMenuTextCheck;
 }
