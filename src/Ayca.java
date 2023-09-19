6c0b5cd65d29ba9d7c8eb4ebe90847c1494e9398
aycadogannn <aycauresin@gmail.com>
2023-09-19 23:03:39 +0300

    test metodlarımı bitirdim

--- src/Ayca.java
+++ src/Ayca.java
@@ -2,27 +2,34 @@ import Utility.BaseDriver;
 import Utility.MyFunc;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Ayca extends BaseDriver {
     @Test
     public void Login_test(){
+        String userMail= "hamzihamda@gmail.com";
+
         driver.get("https://demo.nopcommerce.com/");
+        MyFunc.Wait(2);
         WebElement loginButton =driver.findElement(By.linkText("Log in"));
         loginButton.click();
-
+        MyFunc.Wait(2);
         WebElement eMail=driver.findElement(By.id("Email"));
-        eMail.sendKeys("hamzihamda@gmail.com");
-
+        eMail.sendKeys(userMail);
+        MyFunc.Wait(2);
         WebElement password=driver.findElement(By.id("Password"));
         password.sendKeys("123456789");
-
-        WebElement submitButton=driver.findElement(By.xpath("(//button[@type='submit'])[1]"));
+        MyFunc.Wait(2);
+        WebElement submitButton=driver.findElement(By.xpath("//button[text()='Log in']"));
         submitButton.click();
-
+        MyFunc.Wait(2);
         WebElement myAcc=driver.findElement(By.linkText("My account"));
         myAcc.click();
-        WebElement eMailConfirm=driver.findElement(By.className("valid"));
-        eMailConfirm.getText();
+        WebElement eMailConfirm=driver.findElement(By.cssSelector("input[id='Email']"));
+        MyFunc.Wait(2);
+
+        Assert.assertTrue(userMail.equals(eMailConfirm.getAttribute("value")));
+
     }
 }
